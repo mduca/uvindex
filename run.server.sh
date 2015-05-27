5e6cbf5660afe7bd1a48b396433f0f23cac6e0a1
Felipe <auditor400@gmail.com>
2015-05-27 11:04:18 -0700

    again

--- run.server.sh
+++ run.server.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#make it work in felipe's machine
+#make it work in felipe's machine, again
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform        
     rvm use ruby-2.1.1
